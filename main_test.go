d8ad7b73bbd4792bd67b4e8177cf22f27df756fb
Nikolai Zhogin <nickzhog@gmail.com>
2023-03-30 14:28:45 +0300

    unit test add

--- main_test.go
+++ main_test.go
@@ -2,7 +2,27 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestFindLongestGroup(t *testing.T) {
+	matrix := [][]block{
+		{{0}, {0}, {0}, {0}},
+		{{1}, {0}, {1}, {0}},
+		{{0}, {2}, {1}, {1}},
+		{{0}, {0}, {1}, {0}},
+		{{0}, {2}, {1}, {1}},
+		{{1}, {0}, {1}, {0}},
+		{{1}, {0}, {0}, {0}},
+	}
+	longestGroup, gtype := findLongestGroup(matrix)
+
+	assert := assert.New(t)
+	assert.Equal(TypeVertical, gtype)
+	assert.Equal(1, longestGroup[0].color)
+	assert.Equal(5, len(longestGroup))
+}
 
 func BenchmarkFindLongestGroup(b *testing.B) {
 	height, width, colorsCount := 100, 100, 10
